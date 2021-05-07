320467ee48d11ebdb38d1e7a13a72a80c75636b1
Yuriy Koziy <yukoziy@gmail.com>
2021-05-06 20:14:29 -0400

    Added event handlers

--- src/modules/dom/MainContent.js
+++ src/modules/dom/MainContent.js
@@ -2,15 +2,44 @@ import {renderHTML as renderTodos} from './Todos';
 import {renderHTML as renderAside} from './Aside';
 
 const markup = `
-    <div id="main-content">
     ${renderTodos()}
     ${renderAside()}
-    </div>
 `;
+//edit-task-btn
+function handleTodoClick(event) {
+    const node = event.target.nodeName.toLowerCase();
+    if (node !== 'li') return;
+    event.srcElement.classList.toggle('done');
+}
+
+function editTaskHandler(event) {
+    console.log('Clicked edit task');
+}
+
+function editProjectHandler(event) {
+    console.log('Clicked on edit project');  
+}
+
+function addTaskHandler(event) {
+    console.log('Clicked on add task');  
+}
 
 function renderHTML(parentElement) {
-    // ! not final.....
-    parentElement.insertAdjacentHTML('beforeend', markup);
+    const div = document.createElement('div');
+
+    div.setAttribute('id', 'main-content');
+    div.innerHTML = markup;
+
+    const todos = div.querySelector('.task-todos');
+    todos.addEventListener('click', handleTodoClick);
+
+    const editBtns = div.querySelectorAll('.edit-task-btn');
+    editBtns.forEach(item => item.addEventListener('click', editTaskHandler));
+
+    div.querySelector('#edit-project-btn').addEventListener('click', editProjectHandler);
+    div.querySelector('#add-todo-btn').addEventListener('click', addTaskHandler);
+
+    parentElement.appendChild(div);
 }
 
 export { renderHTML };
--- src/modules/dom/SideNav.js
+++ src/modules/dom/SideNav.js
@@ -2,18 +2,44 @@ import {renderHTML as renderNav} from './Nav';
 import {renderHTML as renderProjects} from './Projects';
 
 const markup = `
-    <div id="side-nav">
     ${renderNav()}
     ${renderProjects()}
     <div id="add-project">
         <div id="add-project-btn">+ New Project</div>
     </div>
-    </div>
 `;
 
+function newProjectHandler(event) {
+    console.log('+ New Project button clicked');
+}
+
+function navHandler(event) {
+    const node = event.target.nodeName.toLowerCase();
+    if (node !== 'li') return;
+    console.log(`Clicked on ${event.target.textContent}`);
+}
+
+function projectHandler(event) {
+    const node = event.target.nodeName.toLowerCase();
+    if (node !== 'li') return;
+    console.log(`Clicked on ${event.target.textContent}`);
+}
+
 function renderHTML(parentElement) {
-    // ! not final.....
-    parentElement.insertAdjacentHTML('afterbegin', markup);
+    const div = document.createElement('div');
+
+    div.setAttribute('id', 'side-nav');
+    div.innerHTML = markup;
+
+    div.querySelector('#add-project-btn').addEventListener('click', newProjectHandler);
+
+    const navMenu = div.querySelector('nav ul');
+    navMenu .addEventListener('click', navHandler);
+
+    const projecstList = div.querySelector('#projects ul');
+    projecstList.addEventListener('click', projectHandler);    
+
+    parentElement.appendChild(div);
 }
 
 export { renderHTML };
