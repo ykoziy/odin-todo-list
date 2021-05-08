57c045aba9d9ca68f86244d0323e83e49e32f17b
Yuriy Koziy <yukoziy@gmail.com>
2021-05-07 22:50:14 -0400

    Clicking on the project opens it, with the tasks

--- src/modules/dom/Todos.js
+++ src/modules/dom/Todos.js
@@ -38,38 +38,63 @@ function renderProjectDetails(title, description) {
     `;
 }
 
-const markup2 = `
-    <div class="project-todos">
-    <div class="task">
-        <div class="task-title">
-            <h2>A task</h2>
-            <div class="edit-task-btn">...</div>
+function generateTaskListItem(task) {
+    return `<li><input type="checkbox" value="done" ${task.isDone ? 'checked' : ''}>${task.title}</li>`;
+}
+
+function taskMarkup(task) {
+    return `
+        <div class="task">
+            <div class="task-title">
+                <h2>${task.title}</h2></span>
+                <div class="edit-task-btn">...</div>
+            </div>                           
         </div>
-        <div class="task-todos">
-            <ul>
-                <li><input type="checkbox" value="done"> todo item 1</li>
-                <li><input type="checkbox" value="done"> todo item 2</li>
-                <li><input type="checkbox" value="done"> todo item 3</li>
-                <li class="done"><input type="checkbox" value="done" checked> todo item 4</li>                                                             
-            </ul>
-        </div>                            
-    </div>
-    <div class="task">
-        <div class="task-title">
-            <h2>A task without todos, checkbox</h2></span>
-            <div class="edit-task-btn">...</div>
-        </div>                           
-    </div>                        
-    </div>
-`;
+    `;
+}
+
+function subtasksMarkup(task) {
+    return `
+        <div class="task">
+            <div class="task-title">
+                <h2>${task.title}</h2>
+                <div class="edit-task-btn">...</div>
+            </div>
+            <div class="task-todos">
+                <ul>
+                ${task.tasks.map(item => generateTaskListItem(item)).join('')}                                                       
+                </ul>
+            </div>                            
+        </div>
+    `;
+}
+
+function renderProjectTasks(tasks) {
+    let div = document.createElement('div');
+    div.classList.add('project-todos');
+
+    let html = '';
+
+    for (const task of tasks) {
+        if (task.constructor.name === 'TaskList') {
+            html += subtasksMarkup(task);
+        } else if (task.constructor.name === 'Task') {
+            html += taskMarkup(task);
+        }
+    }
+    return html;
+}
 
 function renderProjectItem(msg, data) {
     let div = document.querySelector('#main-todos');
-    div.innerHTML = '';
+    let html = `
+        ${renderProjectDetails(data.title, data.description)}
+        <div class="project-todos">
+        ${renderProjectTasks(data.tasks)}
+        </div>
+    `;
 
-    div.innerHTML = renderProjectDetails(data.title, data.description) + markup2;
-
-    console.log(data);
+    div.innerHTML = html;
 }
 
 function renderHTML(parentElement) {
