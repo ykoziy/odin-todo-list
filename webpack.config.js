108aadd16425d560befad3e1bcad3d425a79a651
Yuriy Koziy <yukoziy@gmail.com>
2021-05-07 21:52:31 -0400

    Added TerserWebpackPlugin to preserve class names after JS minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,14 @@ module.exports = {
     filename: './js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+        new TerserPlugin({
+            terserOptions: {
+                keep_classnames: true
+            }
+          })
+        ]
+  },  
 };
