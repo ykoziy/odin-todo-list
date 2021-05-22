8e2284be142c40871f4cdd22ef2f56cff2a2affd
Yuriy Koziy <yukoziy@gmail.com>
2021-05-22 15:55:02 -0400

    Added delete project button

--- src/modules/dom/Aside.js
+++ src/modules/dom/Aside.js
@@ -4,6 +4,9 @@ const markup = `
         <div id="edit-project-btn">
         -
         </div>
+        <div id="delete-project-btn">
+            <i class="fas fa-trash-alt"></i>
+        </div>
     </div>
     <div id="main-aside-bottom">
         <div id="add-todo-btn">
