87e07ab7fd7e52e522382cbd98ccb887d6bc4b9e
Yuriy Koziy <yukoziy@gmail.com>
2021-06-09 20:54:49 -0400

    Fixed styling, added nav menu icons

--- src/modules/dom/Nav.js
+++ src/modules/dom/Nav.js
@@ -5,10 +5,10 @@ const markup = `
     </div>
     <nav>
         <ul>
-            <li data-name="inbox">Inbox</li>
-            <li data-name="upcoming">Upcoming</li>
-            <li data-name="today">Due Today</li>
-            <li data-name="urgent">Urgent</li>
+            <li data-name="inbox" id="inbox-nav"><i class="fas fa-inbox"></i><p>Inbox</p></li>
+            <li data-name="upcoming" id="upcoming-nav"><i class="fas fa-calendar-alt"></i><p>Upcoming</p></li>
+            <li data-name="today" id="today-nav"><i class="fas fa-star"></i><p>Due Today</p></li>
+            <li data-name="urgent" id="urgent-nav"><i class="fas fa-exclamation-circle"></i><p>Urgent</p></li>
         </ul>
     </nav>
     </div>
